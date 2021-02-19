69a2b724e8c8f99f1a32b779832779b158db430c
Evandro Abreu Domingues <Evandroad@github.com>
2021-02-18 23:05:05 -0300

    mais aulas

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente iusto voluptatibus nam, esse sint quisquam dolor, molestiae enim dolores sunt minima consequatur minus provident harum illum, molestias deleniti? Eveniet, sapiente?</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nihil possimus aperiam temporibus necessitatibus, minus delectus laboriosam placeat, doloremque, aliquid id. Libero iste nostrum quidem dicta eos sit est sint.</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit autem soluta eaque consequatur iste. Odio rem blanditiis pariatur dolorem soluta rerum, modi quisquam doloribus corrupti tempore amet? Maiores, voluptates suscipit!</p>
     </div>
 )
